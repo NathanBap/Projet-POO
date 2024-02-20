9719a846d6815b64da6bd02167c587e914106b02
Ksk <thomaspois1707@gmail.com>
2024-02-20 14:42:48 +0100

    Ajout methodes dans Joueurs

--- model/Joueur.java
+++ model/Joueur.java
@@ -1,76 +1,47 @@
 package model;
 import java.util.*;
 
-/**
- * 
- */
 public class Joueur {
 
-    /**
-     * Default constructor
-     */
-    public Joueur() {
+// Attributs
+    private int score;
+    private String nom;
+    private List ListesLettre;
+    
+//Constructeur
+    public Joueur(String nameGiven) {
+        this.score = 0;
+        this.nom = nameGiven;
     }
 
-    /**
-     * 
-     */
-    private int score;
-
-    /**
-     * 
-     */
-    private String nom;
-
-
-
-    /**
-     * 
-     */
+// Méthodes;
+    public int getScore(){
+        return this.score   
+    }
+    public String getNom(){
+        return this.nom  
+    }
     public void jouer() {
         // TODO implement here
     }
-
-    /**
-     * 
-     */
     public void piocherSac() {
         // TODO implement here
     }
-
-    /**
-     * 
-     */
-    public void prendreLettre() {
-        // TODO implement here
+    public void prendreLettre(List Lettre) {
+        this.ListesLettre.add(Lettre)    
     }
-
-    /**
-     * 
-     */
     public void deposerLettre() {
         // TODO implement here
     }
-
-    /**
-     * 
-     */
     public void calculScore() {
         // TODO implement here
     }
-
-    /**
-     * 
-     */
-    public void annulerTour() {
+    public void annulerTour() {}
+        /*/ TODO implement here
+    }
+    public void placerLettre/**(Coordonées + placerLettre et deposerLettre){
         // TODO implement here
     }
-
-    /**
-     * 
-     */
-    public void placerLettre() {
-        // TODO implement here
-    }
+    
 
 }
