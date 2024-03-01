c19df89465c6eb2a8e9eac643537d366b8be9a6c
Nathan Bapin <101987516+NathanBap@users.noreply.github.com>
2024-03-01 16:34:27 +0100

    Init plateau avec bonus

--- model/Plateau.java
+++ model/Plateau.java
@@ -4,19 +4,25 @@ import java.util.*;
 
 public class Plateau {
     //Attributs
-    private List<Case> listCase = new ArrayList<Case>();
+    private Case[][] plateau;
 
     //Constructeur
     public Plateau() {
+        this.plateau = new Case[15][15];
     }
 
     //Getters / Setters
-    public List<Case> getListCase() {
-        return listCase;
+    public Case[][] getPlateau() {
+        return this.plateau;
     }
 
     //Méthodes
-    public void insererLettre() {
+    public void initPlateau() {  // A modifier pour les cases avec des bonus
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                this.plateau[i][j] = new Case(i, j);
+            }
+        }
     }
 
 }
