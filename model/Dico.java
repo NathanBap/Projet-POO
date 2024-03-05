01b25284f5454fe10800a1d8963f801637a9a556
lyna chaba <chabalyna21@gmail.com>
2024-03-05 11:15:32 +0100

    class Dico

---
+++ model/Dico.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.*;
+import java.io.BufferedReader;// lire un texte à partir d'un flux d'entrée caractère
+import java.io.FileReader; //lire des caractères a partir d un fichier
+import java.io.IOException;
+
+public class Dico {
+    // attribut
+    private List<String> dictionnaire;
+
+    public Dico() {
+        dictionnaire = new ArrayList<>();
+        chargerDictionnaire();
+    }
+
+    // charger le dictionnaire depuis un fichier
+    public void chargerDictionnaire() {
+        String nomFichier = "dico_Scrabble.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(nomFichier))) {
+            String mot;
+            // Boucle de lecture des lignes du fichier
+            while ((mot = br.readLine()) != null) {
+                dictionnaire.add(mot.trim());
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+        }
+        // for (int i = 0; i < 20; i++) {
+        // System.out.println(dictionnaire.get(i));
+        // }
+    }
+
+    // vérifier si un mot est valide
+    public boolean estValide(String mot) {
+        return dictionnaire.contains(mot);
+    }
+
+}
