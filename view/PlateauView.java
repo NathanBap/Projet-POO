e477bc6cf3195725165d69ffd33bea9c90527597
Sekou <97668579+ThomasKsK@users.noreply.github.com>
2024-03-06 12:18:24 +0100

    Add files via upload
    Création d'une interface basique pour le plateau

--- view/PlateauView.java
+++ view/PlateauView.java
@@ -1,22 +1,46 @@
 package view;
-import javax.swing.JFrame;
+import model.Plateau;
 
+import javax.swing.*;
+import java.awt.*;
 
 public class PlateauView extends JFrame {
+    private Plateau plateau;
+    
     public PlateauView() {
-        setTitle("Jeu de Scrabble");
-        setSize(800, 600); // Taille de la fenêtre
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Pour fermer l'application
+        initGame();
+        initComponents();
+    }
 
-        GrilleView boardPanel = new GrilleView();
-        add(boardPanel);
-        pack(); // Ajuste la taille de JFrame basée sur le contenu
+    private void initGame() {
+        plateau = new Plateau();
+        plateau.initPlateau();
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Scrabble POO");
+
+        JPanel mainPanel = new JPanel(new GridLayout(15, 15));
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                JLabel label = new JLabel(String.valueOf(plateau.getCase(i, j).getBonus()));
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                mainPanel.add(label);
+            }
+        }
+
+        add(mainPanel);
+        pack();
+        setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            PlateauView frame = new PlateauView();
-            frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            PlateauView plateauView = new PlateauView();
+            plateauView.setVisible(true);
         });
     }
 }
