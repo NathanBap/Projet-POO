6e45424e58c4f00e4aedc7afb59e820ad0f09955
Nathan Bapin <101987516+NathanBap@users.noreply.github.com>
2024-04-02 09:16:04 +0200

    merged Nathan et dev

--- view/CaseView.java
+++ view/CaseView.java
@@ -5,17 +5,57 @@ import model.Lettre;
 import java.awt.*;
 import javax.swing.*;
 
-public class CaseView extends JLayeredPane{
+public class CaseView extends JPanel{
     private Case casee;
     private LettreView lettrePosee;
 
     public CaseView(Case casee) {
         this.casee = casee;
+        int i = casee.getX();
+        int j = casee.getY();
+
         setLayout(new BorderLayout());
-        JLabel label = new JLabel(String.valueOf(casee.getBonus()));
+        JLabel label = new JLabel();
         label.setHorizontalAlignment(SwingConstants.CENTER);
-        add(label, 1);
-        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        // Création d'un panneau pour chaque case
+        // Mettre tout ça dans CaseView
+        setBorder(BorderFactory.createLineBorder(Color.WHITE)); // Bordure blanche
+
+        if (i == 7 && j == 7) {
+            setBackground(Color.PINK);
+
+        } else {
+            // Récupérer le bonus de la case
+            String bonus = casee.getBonus();
+
+            // Modifier la couleur de fond en fonction du bonus
+            switch (bonus) {
+                case "LD":
+                    setBackground(new Color(153, 204, 255)); // Bleu clair
+                    label.setText("<html><center>LETTRE<br>DOUBLE</center></html>");
+                    break;
+                case "LT":
+                    setBackground(new Color(40, 103, 199)); // Bleu foncé
+                    label.setText("<html><center>LETTRE<br>TRIPLE</center></html>");
+                    break;
+                case "MD":
+                    setBackground(new Color(255, 204, 102)); // Saumon clair
+                    label.setText("<html><center>MOT<br>DOUBLE</center></html>");
+                    break;
+                case "MT":
+                    setBackground(new Color(204, 51, 51)); // Rouge foncé
+                    label.setText("<html><center>MOT<br>TRIPLE</center></html>");
+                    break;
+                default:
+                    setBackground(new Color(75, 173, 101));
+            }
+            Font font = new Font(" Ubuntu", Font.BOLD, 12);
+            label.setFont(font);
+
+            // Ajout de l'effet 3D
+            add(label, BorderLayout.CENTER);
+        }  
     }
 
     public Case getCase() {
@@ -24,7 +64,7 @@ public class CaseView extends JLayeredPane{
     public void setLettrePosee(LettreView lettrePosee) {
         //removeAll();
         this.lettrePosee = lettrePosee;
-        add(lettrePosee, 2);
+        add(lettrePosee, BorderLayout.CENTER);
         repaint();
         revalidate();
     }
--- view/PlateauView.java
+++ view/PlateauView.java
@@ -1,6 +1,5 @@
 package view;
 
-import model.Case;
 import model.Plateau;
 
 import javax.swing.*;
@@ -17,8 +16,6 @@ public class PlateauView extends JFrame {
     private java.util.List<CaseView> allCases = new ArrayList<CaseView>();
 
     public LettreView lettreClicked;
-
-    public JPanel lettreClicked;
     
     public PlateauView() {
         initGame();
@@ -47,67 +44,21 @@ public class PlateauView extends JFrame {
             for (int j = 0; j < 15; j++) {
               
                 
-                CaseView panel = new CaseView(plateau.getCase(i, j));
-                panel.addMouseListener(new CaseControler(casee, this));
-              
-                // Création d'un panneau pour chaque case
-                // Mettre tout ça dans CaseView
-                panel.setBorder(BorderFactory.createLineBorder(Color.WHITE)); // Bordure blanche
-              
-                // TMP
-                JLabel label = new JLabel();
+                CaseView casee = new CaseView(plateau.getCase(i, j));
+                casee.addMouseListener(new CaseControler(casee, this));
 
-                if (i == 7 && j == 7) {
-                    panel.setBackground(Color.PINK);
-
-                } else {
-                    // Récupérer le bonus de la case
-                    String bonus = plateau.getCase(i, j).getBonus();
-
-                    // Modifier la couleur de fond en fonction du bonus
-                    switch (bonus) {
-                        case "LD":
-                            panel.setBackground(new Color(153, 204, 255)); // Bleu clair
-                            label.setText("<html><center>LETTRE<br>DOUBLE</center></html>");
-                            break;
-                        case "LT":
-                            panel.setBackground(new Color(40, 103, 199)); // Bleu foncé
-                            label.setText("<html><center>LETTRE<br>TRIPLE</center></html>");
-                            break;
-                        case "MD":
-                            panel.setBackground(new Color(255, 204, 102)); // Saumon clair
-                            label.setText("<html><center>MOT<br>DOUBLE</center></html>");
-                            break;
-                        case "MT":
-                            panel.setBackground(new Color(204, 51, 51)); // Rouge foncé
-                            label.setText("<html><center>MOT<br>TRIPLE</center></html>");
-                            break;
-                        default:
-                            panel.setBackground(new Color(75, 173, 101));
-                    }
-                    Font font = new Font(" Ubuntu", Font.BOLD, 12);
-                    label.setFont(font);
-
-                    // Ajout de l'effet 3D
-                    panel.add(label, BorderLayout.CENTER);
-                }
-                mainPanel.add(panel);
-
+                mainPanel.add(casee);
                 allCases.add(casee);
-
-
+              
             }
         }
-
         // Ajout du panneau principal et du panneau flou à la fenêtre
         setLayout(new BorderLayout());
         add(mainPanel, BorderLayout.CENTER);
-        add(mainPanel, BorderLayout.CENTER);
-
-        add(mainPanel);
         pack();
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //setLocationRelativeTo(null);
+        //setLocationRelativeTo(null);  
+
 
         JPanel footerPanel = new JPanel();
         // Définir la couleur de fond ou ajouter d'autres composants au footerPanel si
@@ -159,5 +110,3 @@ public class PlateauView extends JFrame {
     }
 
 }
-
-// TODO, la pose de pièces sur le plateau
